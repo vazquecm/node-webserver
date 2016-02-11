7d5c633aaa8d575cbb5ec848fa6b66a8b7ddee9b
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-11 16:37:41 -0600

    webscraping complete/commented code

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,5 +1,7 @@
 'use-strict';
 
+const $ = require('jquery');
+
 $('#submit').click(function(){
   console.log('button works');
   window.location.href = `http://www.timeanddate.com/calendar`;
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const express = require('express');
 const app = express();
@@ -7,18 +7,24 @@ const bodyParser = require('body-parser');
 //const upload = require('multer')({ dest: 'tmp/uploads' });
 const multer = require('multer');
 const imgur = require('imgur');
-
-//const request = require('request');
-//const _ = require('lodash');
-//const cheerio = require('cheerio');
+const MongoClient = require('mongodb').MongoClient;
+// actuals request the web page and pulls down the info to be used
+const request = require('request');
+const _ = require('lodash');
+// allows you to look at the "requested" data with jquery like selectors
+const cheerio = require('cheerio');
 
 const path = require('path');
 const sassMiddleware = require('node-sass-middleware');
 const PORT = process.env.PORT || 3000;
+const MONGODB_URL = 'mongodb://localhost:27017/node-webserver';
 
+let db;
 
-// multer = is getting streams of files and then adding them together "multi posts" so you get
-// small bits at a time instead of waiting for a huge file to loa
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// multer = is getting streams of files and then adding them together "multi posts" so you get small bits at a time instead of waiting for a huge file to load
 let newFilename;
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -72,9 +78,18 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 });
 // POST method
-app.post('/contact', (req, res) => {
-  const name = req.body.name;
-  res.send('<h1>Thanks for contacting us ${name}</h1>');
+app.post('/contact', (req) => {
+  const obj = {
+    name: req.body.name,
+    email: req.body.email,
+    message: req.body.message
+  };
+/// this is getting info from the database and inserting it to the contact form
+  db.collection('contact').insertOne(obj, (err, res) => {
+    if (err) throw err;
+
+  res.send('<h1>Thanks for contacting us ${obj.name}</h1>');
+  });
 });
 
 
@@ -84,7 +99,7 @@ app.get('/sendphoto', (req, res) => {
 
 // uploading the new file to Imgur
   imgur.uploadFile('tmp/uploads/' + newFilename)
-    .then(function (json) {
+    .then(function (json, req) {
         console.log(json.data.link);
         console.log(req.file.filename);
     })
@@ -103,66 +118,92 @@ app.post('/sendphoto', upload.single('image'), (req, res) => {
 });
 
 
-// TOP STORIES - getting data from CNN  //
-//const $bannerText = $('.banner-text');  //caching the selector VERY important in frontend, affects run time to all it multiply times as below instead put in variable
+app.post('/api', (req, res) => {
+  const obj = _.mapValues(req.body, val => val.toUpperCase());
 
-//app.get('/api/news', (req,res) => {
-  //const url = `http://cnn.com`;
-  //request.get(url, (err, response, html) => {
-    //if (err) throw err;
-    //const news = [];
-     ////converting html string into a cheerio obj - entire page turned into a giant jquery like object
-    //const $ = cheerio.load(html);
-    //news.push({
-      //title: $('.banner-text').text(),
-      //url: $('.banner-text').closest('a').attr('href')
-    //});
+  db.collection('allcaps').insertOne(obj, (err, result) => {
+    if (err) throw err;
 
-    //const $cdHeadline = $('cd__headline');  //cache array of DOM elements
-
-    //_.range(1,12).forEach(i => {
-      //const $headline = $cdHeadline.eq(i);
-      //news.push({
-        //title: $('.cd__headline').eq(i).text(),
-        //url: url + $headline.find('a').attr('href') //cd headline is a selector cache the selector??
-      //});
-    //});
-    //res.send(news);            //converting back again
-  //});
-//});
+    console.log(result.ops[0]);
+    res.send(obj);
+  });
+});
 
 
-// API //
-//app.use(bodyParser.json());
-//app.get('/api', (req, res) =>  {
-   ////when cb hits first set header then respond hello world
-  //res.header('Access-Contril-Allow-Origin', '*');});
+// getting weather data
+app.get('/api/weather', (req, res) => {
+  const API_KEY = '00c2032f84f5e9393b7a1eda02d49228';
+  const url = `https://api.forecast.io/forecast/${API_KEY}/37.8267,-122.423`;
 
-//app.post('/api', (req, res) => {
-  //console.log(req.body);
-  //const obj = _.mapValues(req.body, val => val.toUpperCase());
-  //res.send(obj);
-//});
+  request.get(url, (err, response, body) => {
+    if (err) throw err;
 
-//app.get('/api/weather', (req,res) => {
-  //const url = ``;
-  //request.get(url, (err, response, body) => {
-    //if (err) throw err;
-    //res.header('Access-Control-Allow-Origin', '*');
-    //res.send(JSON.parse(body));
-  //});
-//});
+    res.header('Access-Control-Allow-Origin', '*');
+    res.send(JSON.parse(body));
+  });
+});
+
+
+// getting top news stories from cnn -- not using an "else" statement is allowing us to not write same code repeatedly
+app.get('/api/news', (req, res) => {
+  db.collection('news').findOne({}, (err, doc) => {
+    if (doc) {
+      const FIFTEEN_MINUTES_IN_MS = 15 * 60 * 1000;
+      const diff = new Date() - doc._id.getTimestamp() - FIFTEEN_MINUTES_IN_MS;
+      const lessThan15MinutesAgo = diff > 0;
+
+      if (lessThan15MinutesAgo) {
+        res.send(doc);
+        return;
+      }
+    }
+      const url = 'http://cnn.com';
+
+      request.get(url, (err, response, html) => {
+        if (err) throw err;
+
+        const news = [];
+        ////converting html string into a cheerio obj - entire page turned into a giant jquery like object
+        const $ = cheerio.load(html);
+        //caching the selector VERY important in frontend, affects run time to all it multiply times as below instead put in variable
+        const $bannerText = $('.banner-text');
+
+        news.push({
+          title: $bannerText.text(),
+          url: url + $bannerText.closest('a').attr('href')
+        });
+        //cache array of DOM elements
+        const $cdHeadline = $('.cd__headline');
+
+        _.range(1, 12).forEach(i => {
+          const $headline = $cdHeadline.eq(i);
+
+          news.push({
+            title: $headline.text(),
+            url: url + $headline.find('a').attr('href')
+          });
+        });
+
+        db.collection('news').insertOne({ top: news }, (err, res) => {
+          if (err) throw err;
+          //converting back again
+          res.send(news);
+        });
+      });
+    })
+  })
+
 
 // ROUTE: hello - getting a name to show
 app.get('/hello', (req, res) => {
- let name = req.query.name;
- // the program will use the "if" statement if no name is being queried
- if (name ===undefined) {
-   name = 'World'
- }
- const msg = `<h1>Hello ${name}!</h1>
-              <h2>Goodbye ${name}!</h2>`;
-  console.log('query params ', req.query);
+  const name = req.query.name;
+  // the program will use the "if" statement if no name is being queried
+    if (name ===undefined) {
+     name = 'World'
+    }
+      const msg = `<h1>Hello ${name}!</h1>
+                  <h2>Goodbye ${name}!</h2>`;
+      console.log('query params ', req.query);
 
 
 // response header
@@ -186,8 +227,8 @@ app.get('/hello', (req, res) => {
 // this would work if I had a calendar working...but I don't
 app.get('/cal/:year/:month', (req,res) => {
   const month = require('node-cal/lib/month');
-  console.log(month.setUpWholeMonth);
-  res.send('<pre>' + month.setUpWholeMonth(req.params.year, req.params.month) + '</pre>');
+   console.log(month.setUpWholeMonth);
+    res.send('<pre>' + month.setUpWholeMonth(req.params.year, req.params.month) + '</pre>');
 });
 
 //  ROUTE: random - generating a random number - use /random after port#
@@ -195,6 +236,8 @@ app.get('/random', (req, res) => {
   res.send(Math.random().toString());
 });
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// Returns a random integer between min (included) and max (excluded)
  function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max-min)) + min;
 }
@@ -203,8 +246,8 @@ app.get('/random', (req, res) => {
 app.get('/random/:min/:max', (req, res) => {
   const min = req.params.min;
   const max = req.params.max;
-  console.log('PARAMS ', req.params);
-  res.send(getRandomInt(+min, +max).toString());
+   console.log('PARAMS ', req.params);
+    res.send(getRandomInt(+min, +max).toString());
  });
 
 // gets all remaining options
@@ -214,7 +257,20 @@ app.get('/secret', (req, res) => {
     .send('Access Denied!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Node.js server started. Listening on port ${PORT}`);
-});
+MongoClient.connect(MONGODB_URL, (err, database) => {
+  if (err) throw err;
 
+  db = database;
+
+/// example only of getting an object from within an array ///
+  // db.collection('docs').insertMany([
+  //   {a : 'b'}, {c : 'd'}, {e : 'f'}
+  // ], (err, res) =>  {
+  //   if (err) throw err;
+  //   console.log(res);
+  // });
+
+  app.listen(PORT, () => {
+    console.log('Node.js server started. Listening on port ${PORT}');
+  })
+})
